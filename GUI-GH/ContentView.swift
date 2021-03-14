498c299517b11ff1f12acce72d2098380bd5064c
ono4794git <66150504+ono4794git@users.noreply.github.com>
2021-03-14 09:00:57 +0900

    Initial Commit

---
+++ GUI-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GUI-GH
+//
+//  Created by Masahiro Ono on 2021/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GUI-GH/GUI_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUI_GHApp.swift
+//  GUI-GH
+//
+//  Created by Masahiro Ono on 2021/03/14.
+//
+
+import SwiftUI
+
+@main
+struct GUI_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
